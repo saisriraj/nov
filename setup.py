716d8d95d84778f8e9123108c52b26a29771588f
sri raj <saisriraj.5@gmail.com>
2024-11-21 00:58:02 -0600

    csv files added data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name = 'fuel_auto',
+    version = "0.0.1",
+    author = 'sriraj',
+    author_email="saisriraj.5@gmail.com",
+    packages= find_packages()
+
+)

dfc6372a77dcf92d1b70288b3422eafaf330b2f2
sri raj <saisriraj.5@gmail.com>
2024-11-21 02:23:26 -0600

    utils updated with foldernames

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from fuel_auto.logger import logging
+import sys
 
 logging.info("Welcome to r custome logging") 
+
+
+from fuel_auto.exception import Auto_Exception
+
+try:
+    a = 8/0
+except Exception as e:
+    raise Auto_Exception(e,sys)
